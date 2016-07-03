1a4aaa11988eb7b7ff5fbbaaebdab006456f7e0f
Claudiu Creanga <danutempyrium@yahoo.com>
2016-07-03 20:52:19 +0100

    first templating

---
+++ Limesharp/Stockists/view/frontend/requirejs-config.js
@@ -0,0 +1,8 @@
+var config = {
+    map: {
+        '*': {
+            stockists: 'Limesharp_Stockists/js/stockists',
+            gmaps: 'async!http://maps.google.com/maps/api/js?v=3&sensor=false'
+        }
+    }
+};
