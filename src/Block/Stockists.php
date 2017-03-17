cdfc5a6dc18d1d94dd8d8313b3ada418f1c1405c
Claudiu Creanga <danutempyrium@yahoo.com>
2017-03-17 00:12:01 +0000

    move the store filter into main

--- src/Block/Stockists.php
+++ src/Block/Stockists.php
@@ -156,10 +156,20 @@ class Stockists extends \Magento\Framework\View\Element\Template
         $collection = $this->stockistsCollectionFactory->create()
             ->addFieldToSelect('*')
             ->addFieldToFilter('status', Stores::STATUS_ENABLED)
-            ->addStoreFilter($this->_storeManager->getStore()->getId())
             ->setOrder('name', 'ASC');
         return $collection;
     }
+	
+    /**
+     * Get store identifier
+     *
+     * @return  string
+     */
+    public function getStoreId()
+    {
+        return $this->_storeManager->getStore()->getId();
+    }
+
     
     /**
      * get an array of country codes and country names: AF => Afganisthan
